32f44df804fa71c1b7734511f778ed39bddd8132
Ndro <ndro.lozano@gmail.com>
2022-11-24 16:14:14 +0100

    test: check if todos match the h1 of the template

---
+++ tests/unit/mainTemplate.spec.ts
@@ -0,0 +1,10 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import MainTemplate from "@/layouts/MainTemplate.vue";
+
+describe("MainTemplate.vue", () => {
+  it("show todos text on the component", () => {
+    const wrapper = mount(MainTemplate);
+    const h1 = wrapper.get("h1");
+    expect(h1.text()).toMatch("todos");
+  });
+});
