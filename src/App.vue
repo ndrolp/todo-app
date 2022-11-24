75034ec41eb5c8653128cd2379259fb35ac7e693
Ndro <ndro.lozano@gmail.com>
2022-11-24 16:05:59 +0100

    feat: create the base template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <div id="app"><MainTemplate></MainTemplate></div>
 </template>
+
+<script lang="ts">
+import MainTemplate from "./layouts/MainTemplate.vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: { MainTemplate },
+})
+export default class App extends Vue {}
+</script>
 
 <style>
 html,
